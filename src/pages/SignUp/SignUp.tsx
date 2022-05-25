7a382c4ab4e6cdd3110fe1e03e948249910e7129
Pedro Henrique Durand Reis <webpedrod@gmail.com>
2022-05-25 18:42:55 -0300

    Feat: pagina de signup

---
+++ src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,7 @@
+import { Auth } from "../../layouts/auth/Auth";
+
+function SignUp() {
+  return <Auth>Formulario de Cadastro</Auth>;
+}
+
+export { SignUp };
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Login } from "../pages/login/Login";
+import { SignUp } from "../pages/SignUp/SignUp";
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/" element={<div>Tela de tarefas</div>} />
       </Routes>
     </Router>
